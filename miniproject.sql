dce224ffee2060c5e2d72f899764b73735e0da7b
NguyenKhacHung22102006 <hungskyok06@gmail.com>
2026-05-07 13:50:05 +0700

    Add files via upload

--- miniproject.sql
+++ miniproject.sql
@@ -1,56 +1,49 @@
 CREATE DATABASE Shopmanager;
 USE Shopmanager;
 CREATE TABLE Categories(
-	category_id int auto_increment primary key,
-    category_name VARCHAR(255) NOT NUll
+    category_id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL
 );
 CREATE TABLE Product(
-	product_id int auto_increment primary key,
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
     product_name VARCHAR(255) NOT NULL,
-    product_price decimal(10,2) NOT NULL CHECK (price >= 0) ,
-    product_stock int NOT NULL CHECK (stock  >= 0),
+    product_price DECIMAL(10,2) NOT NULL CHECK (product_price >= 0),
+    product_stock INT NOT NULL CHECK (product_stock >= 0),
     category_id INT NOT NULL,
-    constraint categories foreign key (category_id) references category (category_id)
+
+    CONSTRAINT fk_category
+    FOREIGN KEY (category_id)
+    REFERENCES Categories(category_id)
 );
-INSERT INTO categories(category_name)value
+
+INSERT INTO Categories(category_name)
+VALUES
 ('Điện tử'),
-('Thời Trang');
+('Thời trang');
 
- INSERT INTO product(product_id, product_name, product_price, product_stock, category_id)value
- (1, 'iPhone 15', ' 25000000 ' , 10 , 1),
- (1, 'Samsung S23', ' 20000000 ' , 5 , 1),
- (1, 'Áo sơ mi nam ', ' 500000  ' , 50 , 2),
- (1, 'Giày thể thao', ' 1200000 ' , 20 , 2);
- 
- UPDATE products
- SET product_price = '26000000'
- WHERE product_name = 'iPhone15';
- 
- UpDATE products
- SET stock = stock+10
- WHERE category_id = 1;
- 
- DELETE FROM products 
- WHERE product_id = 4;
+INSERT INTO Product(product_name, product_price, product_stock, category_id)
+VALUES
+('iPhone 15', 25000000, 10, 1),
+('Samsung S23', 20000000, 5, 1),
+('Áo sơ mi nam', 500000, 50, 2),
+('Giày thể thao', 1200000, 20, 2);
 
-DELETE FROM products
+UPDATE Product
+SET product_price = 26000000
+WHERE product_name = 'iPhone 15';
+
+UPDATE Product
+SET product_stock = product_stock + 10
+WHERE category_id = 1;
+
+DELETE FROM Product
+WHERE product_id = 4;
+
+DELETE FROM Product
 WHERE product_price < 1000000;
 
-SELECT * FROM products;
-SELECT * FROM categories;
+SELECT * FROM Product;
+SELECT * FROM Categories;
 
-SELECT p.product_id, p.product_name, p.product_price
-FROM products p;
-
-
-
-
-
-
- 
- 
- 
- 
- 
- 
- 
+SELECT p.product_id,p.product_name,p.product_price
+FROM Product p;
